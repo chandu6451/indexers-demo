b92947e7b8e56a92d49cff117d49504ae366ca26
chandu6451 <43878555+chandu6451@users.noreply.github.com>
2018-10-06 12:45:50 +0530

    Add files via upload

---
+++ Calculate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexersDemo
+{
+    public class Calculate
+    {
+        int[] arr;
+
+        public Calculate(int size)
+        {
+            arr = new int[size];
+        }
+
+        public int this[int index]
+        {
+            get { return arr[index]; }
+            set { arr[index] = value; }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexersDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculate cal = new Calculate(10);
+
+            cal[3] = 56;
+            cal[9] = 89;
+            cal[0] = 104;
+
+            Console.WriteLine("Array Elements : ");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(cal[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
